d2237932a1797185027c1d3257a7afe85a755264
Amogh Kulkarni <amogh5533@gmail.com>
2025-03-31 08:52:31 +0530

    created userModel

---
+++ backend/db/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const connectDB = async ()=>{
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Database connected successfully  : ${conn.connection.host}`)
+
+    }
+    catch(err)
+    {
+        console.log("error :",err)
+        process.exit(1)
+    }
+}
+export default connectDb;
---
+++ backend/models/userModel.js
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const userSchema = new mongoose.Schema({
+
+    fullname : {
+
+        firstname :{
+            type : String, 
+            required : true,
+            minlength : [5, 'firstname must be atleast 5 characters'],
+        },
+
+        lastname :{
+            type : String, 
+            required : true,
+            minlength : [5, 'lastname must be atleast 5 characters'],
+        },
+
+    },
+
+    email : {
+            type : String, 
+            required : true
+    }, 
+    password:{
+            type : String,
+            required : true,
+            select : false
+    },
+
+    socketId : {
+        type : String, 
+        required : true
+    }
+},  { timestamps: true })
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign(
+        {_id : this._id}, 
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+    );
+    return token;
+}
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password, this.password);
+
+}
+
+userSchema.statics.hashPassword = async function (password){
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+export default userModel;
