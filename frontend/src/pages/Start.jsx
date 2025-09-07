b27de45b7352f9665631bec988cce727c111f5e5
Amogh Kulkarni <amogh5533@gmail.com>
2025-09-07 16:11:41 +0530

    Logo

--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -6,7 +6,7 @@ const Start = () => {
   return (
     <div>
       <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1619059558110-c45be64b73ae?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8 flex justify-between flex-col w-full'>
-        {/* <img className='w-16 ml-8' src="https://cdn-assets-eu.frontify.com/s3/frontify-enterprise-files-eu/eyJwYXRoIjoid2VhcmVcL2ZpbGVcLzhGbTh4cU5SZGZUVjUxYVh3bnEyLnN2ZyJ9:weare:F1cOF9Bps96cMy7r9Y2d7affBYsDeiDoIHfqZrbcxAw?width=1200&height=417" alt="" /> */}
+        <img className='w-16 ml-8' src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cleanpng.com%2Ffree%2Frideways-logo.html&psig=AOvVaw3pYhQHy2F4KH6TFN26L-rp&ust=1757328035465000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCICbpJ67xo8DFQAAAAAdAAAAABAE" alt="" />
         <div className='bg-white pb-8 py-4 px-4'>
           <h2 className='text-[30px] font-semibold'>Get Started with Ride Ways</h2>
           <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5'>Continue</Link>
